9217854e474b3fbc92e90ddf5da2747a766e8334
Sabrina <sabrinayu7@gmail.com>
2017-05-26 12:23:46 -0400

    Made reset function and button

--- Tortoise_Hare_Simulation/Form1.Designer.cs
+++ Tortoise_Hare_Simulation/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.btnStartRace = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,11 +55,22 @@
             this.btnStartRace.UseVisualStyleBackColor = true;
             this.btnStartRace.Click += new System.EventHandler(this.btnStartRace_Click);
             // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(496, 34);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(696, 375);
+            this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnStartRace);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
@@ -73,6 +85,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button btnStartRace;
+        private System.Windows.Forms.Button btnReset;
     }
 }
 
