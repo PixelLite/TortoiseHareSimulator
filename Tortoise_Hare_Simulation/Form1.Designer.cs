a8eb4ab2228598256df3f122e35559faebc84077
Tanya <tanyaqnguyen@hotmail.com>
2017-05-30 13:17:04 -0400

    file - save as and open

--- Tortoise_Hare_Simulation/Form1.Designer.cs
+++ Tortoise_Hare_Simulation/Form1.Designer.cs
@@ -43,14 +43,23 @@
             this.label2 = new System.Windows.Forms.Label();
             this.btnNewGame = new System.Windows.Forms.Button();
             this.btnPause = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.binaryFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textFileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.binaryFileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trckSpeed)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 105);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(700, 100);
             this.pictureBox1.TabIndex = 0;
@@ -59,7 +68,7 @@
             // 
             // btnStartRace
             // 
-            this.btnStartRace.Location = new System.Drawing.Point(208, 118);
+            this.btnStartRace.Location = new System.Drawing.Point(204, 211);
             this.btnStartRace.Name = "btnStartRace";
             this.btnStartRace.Size = new System.Drawing.Size(75, 23);
             this.btnStartRace.TabIndex = 1;
@@ -69,7 +78,7 @@
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(374, 118);
+            this.btnReset.Location = new System.Drawing.Point(370, 211);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(75, 23);
             this.btnReset.TabIndex = 2;
@@ -117,7 +126,7 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.lblScoreT);
-            this.groupBox1.Location = new System.Drawing.Point(479, 118);
+            this.groupBox1.Location = new System.Drawing.Point(475, 211);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(108, 70);
             this.groupBox1.TabIndex = 4;
@@ -127,14 +136,14 @@
             // lboxCommentary
             // 
             this.lboxCommentary.FormattingEnabled = true;
-            this.lboxCommentary.Location = new System.Drawing.Point(16, 118);
+            this.lboxCommentary.Location = new System.Drawing.Point(12, 211);
             this.lboxCommentary.Name = "lboxCommentary";
             this.lboxCommentary.Size = new System.Drawing.Size(120, 69);
             this.lboxCommentary.TabIndex = 5;
             // 
             // trckSpeed
             // 
-            this.trckSpeed.Location = new System.Drawing.Point(479, 212);
+            this.trckSpeed.Location = new System.Drawing.Point(475, 305);
             this.trckSpeed.Minimum = 1;
             this.trckSpeed.Name = "trckSpeed";
             this.trckSpeed.Size = new System.Drawing.Size(104, 45);
@@ -144,7 +153,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(479, 193);
+            this.label2.Location = new System.Drawing.Point(475, 286);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 7;
@@ -152,7 +161,7 @@
             // 
             // btnNewGame
             // 
-            this.btnNewGame.Location = new System.Drawing.Point(289, 118);
+            this.btnNewGame.Location = new System.Drawing.Point(285, 211);
             this.btnNewGame.Name = "btnNewGame";
             this.btnNewGame.Size = new System.Drawing.Size(75, 23);
             this.btnNewGame.TabIndex = 8;
@@ -162,7 +171,7 @@
             // 
             // btnPause
             // 
-            this.btnPause.Location = new System.Drawing.Point(208, 118);
+            this.btnPause.Location = new System.Drawing.Point(204, 211);
             this.btnPause.Name = "btnPause";
             this.btnPause.Size = new System.Drawing.Size(75, 23);
             this.btnPause.TabIndex = 9;
@@ -170,11 +179,76 @@
             this.btnPause.UseVisualStyleBackColor = true;
             this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(728, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveAsToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.textFileToolStripMenuItem,
+            this.binaryFileToolStripMenuItem});
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            // 
+            // textFileToolStripMenuItem
+            // 
+            this.textFileToolStripMenuItem.Name = "textFileToolStripMenuItem";
+            this.textFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.textFileToolStripMenuItem.Text = "Text File";
+            this.textFileToolStripMenuItem.Click += new System.EventHandler(this.textFileToolStripMenuItem_Click);
+            // 
+            // binaryFileToolStripMenuItem
+            // 
+            this.binaryFileToolStripMenuItem.Name = "binaryFileToolStripMenuItem";
+            this.binaryFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.binaryFileToolStripMenuItem.Text = "Binary File";
+            this.binaryFileToolStripMenuItem.Click += new System.EventHandler(this.binaryFileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.textFileToolStripMenuItem1,
+            this.binaryFileToolStripMenuItem1});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // textFileToolStripMenuItem1
+            // 
+            this.textFileToolStripMenuItem1.Name = "textFileToolStripMenuItem1";
+            this.textFileToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.textFileToolStripMenuItem1.Text = "Text File";
+            this.textFileToolStripMenuItem1.Click += new System.EventHandler(this.textFileToolStripMenuItem1_Click);
+            // 
+            // binaryFileToolStripMenuItem1
+            // 
+            this.binaryFileToolStripMenuItem1.Name = "binaryFileToolStripMenuItem1";
+            this.binaryFileToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.binaryFileToolStripMenuItem1.Text = "Binary File";
+            this.binaryFileToolStripMenuItem1.Click += new System.EventHandler(this.binaryFileToolStripMenuItem1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(728, 269);
+            this.ClientSize = new System.Drawing.Size(728, 320);
             this.Controls.Add(this.btnNewGame);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.trckSpeed);
@@ -184,6 +258,8 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Tortoise and Hare Race";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -191,6 +267,8 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trckSpeed)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -212,6 +290,14 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnNewGame;
         private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem binaryFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textFileToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem binaryFileToolStripMenuItem1;
     }
 }
 
